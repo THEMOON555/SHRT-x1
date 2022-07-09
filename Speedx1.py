ff005524cf9ce89a3fd73957cbbc05229402a2e3
TheDemon <108716811+THEMOON555@users.noreply.github.com>
2022-07-09 12:26:27 +0800

    Update Speedx1.py

--- Speedx1.py
+++ Speedx1.py
@@ -40,9 +40,9 @@ def bitly():
     BITLY_ACCESS_TOKEN = "74e40eae5737852ee2bba8ece28adb3b452208c4"
     link = input(f"{biru}[~] Link : ")
     start = bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
-    response = b.shorten('http://google.com/')
+    response = start.shorten('http://google.com/')
 print(response)
-    done = start.shorten(link)
+   
     time.sleep(3)
     printtik(f"[✓] 'message':'success-shortn'")
     printtik (f"""
